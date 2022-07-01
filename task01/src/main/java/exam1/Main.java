b5cb192a7d2f61fccf3a9ed8e9575458fd111441
Nursakinah <nursakinah.arz@gmail.com>
2022-07-01 14:36:42 +0800

    2nd commit

--- task01/src/main/java/exam1/Main.java
+++ task01/src/main/java/exam1/Main.java
@@ -1,8 +1,32 @@
 package exam1;
 
-public class Main {
-    public static void main (String[]args) {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        FileReader fr = new FileReader("/Users/nursakinah/Downloads/thankyou.csv");
+       
+        BufferedReader br = new BufferedReader(fr);
+        String line = "";
+        line = line.replaceAll("[^a-zA-Z0-9]", " ");
         
+        try {
+
+            while ((line=br.readLine()) != null) {
+                System.out.println(line);
+                
+            }
+            
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+        fr.close();
     }
 }
+           
+

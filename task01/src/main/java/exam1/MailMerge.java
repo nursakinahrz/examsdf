edc58e87ac1f46a01de4f5274460dc8cd6b7e533
Nursakinah <nursakinah.arz@gmail.com>
2022-07-01 16:13:54 +0800

    6th commit

--- task01/src/main/java/exam1/MailMerge.java
+++ task01/src/main/java/exam1/MailMerge.java
@@ -4,12 +4,16 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class Main {
+public class MailMerge {
     public static void main(String[] args) throws IOException {
 
         FileReader fr = new FileReader("/Users/nursakinah/Downloads/thankyou.csv");
+        FileReader fr2 = new FileReader ("/Users/nursakinah/Downloads/tour_packages.csv");
+        
        
         BufferedReader br = new BufferedReader(fr);
+        BufferedReader br2 = new BufferedReader(fr2);
+
         String line = "";
         line = line.replaceAll("[^a-zA-Z0-9]", " ");
         
@@ -24,6 +28,19 @@ public class Main {
 
             e.printStackTrace();
         }
+
+        try {
+
+            while ((line=br2.readLine()) != null) {
+                System.out.println(line);
+                
+            }
+            
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
 
         fr.close();
     }
